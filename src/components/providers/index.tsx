d0dc3d04c73b129ba11f8db2271fc4bb84f6d537
jameelsocorro <github@jisoc.xyz>
2024-05-25 14:54:25 +0800

    setup theme provider

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { ThemeProvider } from "./theme-provider";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
